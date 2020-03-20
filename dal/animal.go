6ec6540d4cf5faa97d7a97a585a1996d6040e208
Chris Johnson <chrislmjohnson@gmail.com>
2020-03-19 22:58:17 -0600

    refactor for better DB access.  now with Dependancy Injection

--- dal/animal.go
+++ dal/animal.go
@@ -1,48 +1,30 @@
 package dal
 
 import (
-	"database/sql"
 	"fmt"
 
 	_ "github.com/lib/pq" //We need a DB
 )
 
-//DB object
+//Animal is the DB object
 type Animal struct {
 	ID    int
 	Name  string
 	AddDt string
 }
 
-//Get an aminal
-func Get(id int) Animal {
-	psqlConn := "host=localhost port=5432 user=postgres password=devpwd dbname=postgres sslmode=disable"
-	db, err := sql.Open("postgres", psqlConn)
-	if err != nil {
-		fmt.Println("Could not connect to DB")
-		fmt.Println(err)
-		panic(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("Could not Ping DB")
-		fmt.Println(err)
-		panic(err)
-	}
-
+//GetAnimal an animal
+func (db *DB) GetAnimal(id int) (Animal, error) {
 	var anAnimal Animal
 
 	row := db.QueryRow(`SELECT id, name, add_dt FROM animal WHERE id =$1`, id)
-	err = row.Scan(&anAnimal.ID, &anAnimal.Name, &anAnimal.AddDt)
+	err := row.Scan(&anAnimal.ID, &anAnimal.Name, &anAnimal.AddDt)
 
-	fmt.Printf("Retrived: %+v", anAnimal)
+	fmt.Printf("Retrived: %+v\n", anAnimal)
 	if err != nil {
 		fmt.Println("query for animal failed")
 		fmt.Println(err)
-		panic(err)
 	}
 
-	return anAnimal
+	return anAnimal, err
 }
--- main.go
+++ main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
-	"go-web-server/handlers"
+	"fmt"
+	"go-web-server/dal"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
+type Env struct {
+	db dal.Datastore
+}
+
 func main() {
+	db, err := dal.NewDB("host=localhost port=5432 user=postgres password=devpwd dbname=postgres sslmode=disable")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	env := &Env{db}
+
 	router := mux.NewRouter()
 
-	router.HandleFunc("/animal/{id}", handlers.GetAnimal).Methods("GET")
+	router.HandleFunc("/animal/{id}", env.GetAnimal).Methods("GET")
 
 	http.ListenAndServe(":8081", router)
+}
+
+//GetAnimal an Animal
+func (env *Env) GetAnimal(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Getting Animal")
+	pathParams := mux.Vars(r)
+
+	animalID, _ := strconv.Atoi(pathParams["id"])
+	fmt.Println(animalID)
+	anAnimal, err := env.db.GetAnimal(animalID)
+	if err != nil {
+		panic(err)
+	}
+
+	message := "This is an " + anAnimal.Name
+
+	w.Write([]byte(message))
 }
